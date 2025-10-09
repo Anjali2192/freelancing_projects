7dd100aaf77234b522e6eba92f74f81bd370cc47
Anjali2192 <967anjalimishra@gmail.com>
2025-10-09 18:09:02 +0530

    Added multiple queries to the script

--- Project 1/google.py
+++ Project 1/google.py
@@ -2,18 +2,38 @@ import requests
 import os
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
+KEYWORDS = ["blogging tips", "marketing strategies", "content creation"]
+
+website_urls = []
+
 API_KEY = os.getenv("google_KEY")
 CX = "23b320d001037432a"
-query = "blogging tips"
-url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={API_KEY}&cx={CX}"
 
-response = requests.get(url)
-data = response.json()
+for keyword in KEYWORDS:
+    print(f"{keyword} is being searched")    
 
-for item in data["items"]:
-    print(item["title"])
-    print(item["link"])
-    print("---")
+    url = f"https://www.googleapis.com/customsearch/v1?q={keyword}&key={API_KEY}&cx={CX}" 
+    
+    response = requests.get(url)
+    data = response.json()
+
+    urls = []
+    for item in data["items"]:
+        urls.append(item["link"])  
+
+    # Store data
+    for link in urls:
+        website_urls.append({
+            "URL": link,
+            "Search Engine": "Bing",
+            "Keyword": keyword,
+            "Date": pd.Timestamp.today().strftime("%Y-%m-%d")
+        })    
+
+    print(f"✅ Found {len(urls)} links for '{keyword}'")    
+
+print(website_urls)
