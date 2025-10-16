899936de7697973f411f36752751da5e610924b5
Anjali2192 <967anjalimishra@gmail.com>
2025-10-17 05:02:09 +0530

    updated the script

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,21 +1,14 @@
 import requests
-import os
-from dotenv import load_dotenv
+import json
+from bs4 import BeautifulSoup
 
-load_dotenv()
-
-url = "https://www.bigbasket.com/basketService/get/"
+url = "https://www.bigbasket.com/cl/fruits-vegetables/?nc=nb&page=1"
 
 headers = {
-    "content-type": "application/json",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
 }
 
-cookies_str = os.getenv("COOKIES")
-cookies = dict(item.split("=", 1) for item in cookies_str.split("; ") if "=" in item)
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.text, "lxml")
 
-# Make the GET request
-response = requests.get(url, headers=headers, cookies=cookies)
-
-# Check the response
-print(response.status_code)
+# Find image urls
